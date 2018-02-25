4dfc2342b869e921e4c6a6f083d2e6e6e96d63cb
Claudia <claudi_a14@hotmail.com>
2018-02-25 17:27:02 -0400

    reacion del archivo AbstractFactory

---
+++ Factory/FactoryMethod/AbstractFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using factoryMethod.Properties;
+
+
+namespace factoryMethod
+{
+   public abstract class AbstractFactory
+    {
+        public static readonly int audio = 1;
+        public static int video = 2;
+
+        public abstract IArchivo crear(int tipo);
+        
+
+    }
+}
