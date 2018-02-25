d81ab9866bac94283561829fc84ff729b87160c0
Claudia <claudi_a14@hotmail.com>
2018-02-25 17:39:08 -0400

    Creacion del archivo ConcrectFactory

---
+++ Factory/FactoryMethod/ConcretFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using factoryMethod.Properties;
+
+
+
+namespace factoryMethod
+{
+    public class ConcretFactory:AbstractFactory
+    {       
+        public override IArchivo crear(int tipo)
+        {
+            IArchivo objeto;
+            switch (tipo)
+            {
+                
+                case 1:
+                   objeto = new ConcrectProductArchAudio();
+                    return objeto;
+                    break;
+                case 2:
+                    return new ConcrectProductArchVideo();                   
+                default:
+                    return null;
+            }            
+        }
+    }
+}
