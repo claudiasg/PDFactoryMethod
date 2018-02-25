d4255af6dca060037dfeb69bb557fee099ff7402
Claudia <claudi_a14@hotmail.com>
2018-02-25 17:36:16 -0400

    Creacion del archivo ConcrectProductArchVideo

---
+++ Factory/FactoryMethod/ConcrectProductArchVideo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace factoryMethod
+{
+   public class ConcrectProductArchVideo: IArchivo
+    {
+        public void Reproducir() {
+            MessageBox.Show("Reproduciendo un archivo de video......");
+        } 
+            
+    }
+}
