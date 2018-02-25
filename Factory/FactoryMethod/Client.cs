e808af94353ceb35c9b9676076993959a8d5cf46
Claudia <claudi_a14@hotmail.com>
2018-02-25 17:50:28 -0400

    Creacion del archivo Cliente

---
+++ Factory/FactoryMethod/Client.cs
@@ -0,0 +1,10 @@
+﻿static void Main()
+        {
+            AbstractFactory factory = new ConcretFactory();
+            IArchivo audio = factory.crear(ConcretFactory.audio);
+            audio.Reproducir();
+
+	    IArchivo video = factory.crear(ConcretFactory.video);
+            video.Reproducir();
+        }
+
